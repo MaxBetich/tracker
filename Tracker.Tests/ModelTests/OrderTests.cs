c4639a53ba7bdb5fee8f14cfcc227bf165d1e657
Max Betich <betichm@gmail.com>
2023-05-12 09:21:22 -0700

    add constructor testing for Order class

---
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Tracker.Models;
+using System;
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
