6464e80578c332b90a3df0a0a5fe3df2f73253ae
Max Betich <betichm@gmail.com>
2023-05-12 10:34:38 -0700

    add further testing to GetAll method of Order object

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,13 @@ using System;
 namespace Tracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
@@ -69,6 +74,24 @@ namespace Tracker.Tests
     public void GetAll_ReturnsEmptyList_OrderList()
     {
       List<Order> newList = new List<Order> {};
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfOrders_OrderList()
+    {
+      string name1 = "test name";
+      string description1 = "test description";
+      DateOnly date1 = new DateOnly(2023,1,1);
+      int price1 = 3;
+      Order newOrder1 = new Order(name1, description1, date1, price1);
+      string name2 = "test name2";
+      string description2 = "test description2";
+      DateOnly date2 = new DateOnly(2023,1,2);
+      int price2 = 4;
+      Order newOrder2 = new Order(name2, description2, date2, price2);
+      List<Order> newList = new List<Order> {newOrder1, newOrder2};
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -9,12 +9,26 @@ namespace Tracker.Models
     public string Description{get;set;}
     public DateOnly Date{get;set;}
     public int Price{get;set;}
+    private static List<Order> _instances = new List<Order> {};
+    public int Id{get;}
     public Order(string name, string description, DateOnly date, int price)
     {
       Name = name;
       Description = description;
       Date = date;
       Price = price;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    // public static List<Order> GetAll()
+    // {
+    //   return _instances;
+    // }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
